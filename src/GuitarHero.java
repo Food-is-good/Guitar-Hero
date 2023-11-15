c78de62b1cd1582becb716d937e2140f536205b7
Food-Is-Good <Henry.zhangyy.sh@gmail.com>
2023-11-15 10:38:16 -0600

    a
    a

--- src/GuitarHero.java
+++ src/GuitarHero.java
@@ -1,56 +1,33 @@
 
 public class GuitarHero {
 	
-	public static String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' "; // String ends with a space
-	public static GuitarString[] ta;
-	public static char key;
+	public String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' "; // String ends with a space
+	public GuitarString[] ta;
+	public char key;
 	
-	public static void main(String[] args) {
-		
+	public GuitarHero () {
 		ta = new GuitarString[keyboard.length()];
 		
 		for(int i = 0; i<keyboard.length(); i++) {
 			ta[i] = new GuitarString(440*Math.pow(1.05956, i-25));
 		}
-		
-        // Create two guitar strings, for concert A and C
-        /*double CONCERT_A = 440.0;
-        double CONCERT_C = CONCERT_A * Math.pow(2, 3.0/12.0);
-        GuitarString stringA = new GuitarString(CONCERT_A);
-        GuitarString stringC = new GuitarString(CONCERT_C);
-        */
-
-        final double TEXT_POS_X = .2;
-        final double TEXT_POS_Y = .5;
-        
-        StdDraw.text(TEXT_POS_X, TEXT_POS_Y, "Type a character to play a note!");
-        
-        play(ta);
     }
     
-    private static void play(GuitarString[] t) {        // the main input loop
-        while (true) {
+    public void play(char key) {        // the main input loop
         	
         	int temp = 0;
-            // check if the user has typed a key, and, if so, process it
-            if (StdDraw.hasNextKeyTyped()) {
- 
-                // the user types this character
-                key = StdDraw.nextKeyTyped();
-                
-                if(keyboard.indexOf(String.valueOf(key))!=-1) {
-                	temp = keyboard.indexOf(String.valueOf(key));
-                    System.out.println(temp);
 
-                    t[temp].pluck();
-                }
-                    
+            if(keyboard.indexOf(String.valueOf(key))!=-1) {
+            	temp = keyboard.indexOf(String.valueOf(key));
+            	System.out.println(temp);
+            	ta[temp].pluck();
             }
+
             
             double sample = 0;
             // compute the superposition of the samples
             for(int i = 0; i<keyboard.length(); i++){
-            	sample+=t[i].sample();
+            	sample+=ta[i].sample();
             }
 
             // send the result to standard audio
@@ -58,10 +35,8 @@ public class GuitarHero {
 
             // advance the simulation of each guitar string by one step
             for(int i = 0; i<keyboard.length(); i++){
-            	t[i].tic();
+            	ta[i].tic();
             }
             
         }
-        
-    }
 }
